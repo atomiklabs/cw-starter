1c041c4909fcc1e97427a1009cec875d09553084
Tomasz Kopacki <tomasz@kopacki.net>
2021-09-07 12:16:46 +1000

    refactor(structure): flatten tree
    This change aims to flatten the project directory tree to keep things simple. I also included some supporting documentation, and made couple of `Makefiles` to simplify running commands.

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -1,3 +1,6 @@
+# Start in ./scripts/.. (so in fact the root)
+cd "$(dirname "$0")/.."
+
 docker run --rm -v "$(pwd)":/code \
 --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
 --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
