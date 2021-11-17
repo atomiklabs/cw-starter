521ec2cfa6304710771dcaa1bf312c4cbb4544b4
korzewski <korzewski@gmail.com>
2021-11-17 13:25:04 +0100

    add a contract version to empty-template

--- contracts/empty-template/src/contract.rs
+++ contracts/empty-template/src/contract.rs
@@ -1,10 +1,14 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cw2::set_contract_version;
 
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 use crate::state::{State, STATE};
+
+const CONTRACT_NAME: &str = "crates.io:{{project-name}}";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -13,6 +17,8 @@ pub fn instantiate(
     _info: MessageInfo,
     _msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
     let state = State {
         
     };
